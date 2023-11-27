1ac2f7b3e6530eb03a36e8fc36af0b3ce37e992a
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-27 18:43:31 -0500

    Add code to index page.

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,12 @@
+import SupportGroups from "../Components/SupportGroups";
+
+function Index() {
+  return (
+    <div className="Index">
+      <h2>Index</h2>
+      <SupportGroups />
+    </div>
+  );
+}
+
+export default Index;
