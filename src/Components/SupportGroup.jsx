d353934c30578971986b2f5706793f116d72eba7
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-26 22:37:34 -0500

    Add code to SupportGroup.jsx.

---
+++ src/Components/SupportGroup.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function SupportGroup({ support_group }) {
+  return (
+    <tr>
+      <td>
+        {support_group.is_favorite ? (
+          <span>👍🏾</span>
+        ) : (
+          <span>&nbsp; &nbsp; &nbsp;</span>
+        )}
+      </td>
+      <td style={{ cursor: "alias" }}>
+        <a href={support_group.location} target="_blank" rel="noreferrer">
+          {support_group.name}
+        </a>
+      </td>
+      <td>
+        <Link to={`/support_groups/${support_group.id}`}>✌🏾</Link>
+      </td>
+    </tr>
+  );
+}
+
+export default SupportGroup;
