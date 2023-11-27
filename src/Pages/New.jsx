7aeec869f45770f202bfaf095447d03932c63023
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-27 18:51:45 -0500

    Add code to new page.

---
+++ src/Pages/New.jsx
@@ -0,0 +1,12 @@
+import SupportGroupNewForm from "../Components/SupportGroupNewForm";
+
+function New() {
+  return (
+    <div className="New">
+      <h2>New</h2>
+      <SupportGroupNewForm />
+    </div>
+  );
+}
+
+export default New;
