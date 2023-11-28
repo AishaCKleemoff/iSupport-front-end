ea4b7fd0ea2a034d61f9696bc393b67a086b9811
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-27 19:02:34 -0500

    Add code to show page

---
+++ src/Pages/Show.jsx
@@ -0,0 +1,12 @@
+import SupportGroupDetails from "../Components/SupportGroupDetails";
+
+function Show() {
+  return (
+    <div className="Show">
+      <h2>Show</h2>
+      <SupportGroupDetails />
+    </div>
+  );
+}
+
+export default Show;
